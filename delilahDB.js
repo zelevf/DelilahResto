59c2319e49c8370025d6a91e2d625a2e7475e06b
zelevf <62726484+zelevf@users.noreply.github.com>
2020-10-05 18:16:48 -0300

    Algunas rutas listas

--- delilahDB.js
+++ delilahDB.js
@@ -2,32 +2,42 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root:@localhost:3306/Delilah');
 
-module.exports = sequelize;
+
 
 
 // ----------------------------- CLIENTE -----------------------------------
-// sequelize.query('select * From cliente', 
-//     {type: sequelize.QueryTypes.SELECT}
-// ).then(function(resultados) {
-//     console.log(resultados)
-// });
 
+// MOSTRAR TODOS LOS CLIENTES
+function clientesDB() {
+    sequelize.query('select * From cliente',
+        { type: sequelize.QueryTypes.SELECT }
+    ).then(function (resultados) {
+        res.json(resultados)
+    });
+}
 
+function agregarUsuariosDB() {
 
+}
 
 // // ----------------------------- PRODUCTOS -----------------------------------
-// sequelize.query('select * From Productos', 
-//     {type: sequelize.QueryTypes.SELECT}
-// ).then(function(resultados) {
-//     console.log(resultados)
-// });
+
+// MOSTRAR TODOS LOS PRODUCTOS
+function productosDB() {
+    sequelize.query('select * From Productos',
+        { type: sequelize.QueryTypes.SELECT }
+    ).then(function (resultados) {
+        res.json(resultados)
+    });
+}
+
+// AGREGAR UN PRODUCTO
+function agregarProductosDB() {
+
+}
 
 
-
-
-
-
-// ----------------------------- PRODUCTOS -----------------------------------
+// ----------------------------- PEDIDOS -----------------------------------
 
 
 // ----------------------------- CARRITO -----------------------------------
@@ -36,4 +46,4 @@ module.exports = sequelize;
 // ----------------------------- ESTADOS DE ÓRDENES -----------------------------------
 
 
-
+module.exports = { };
--- server.js
+++ server.js
@@ -1,42 +1,194 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const server = express();
 
-server.use(bodyParser.json);
+const server = express();
+// const dataBase = require('./delilahDB');
+
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:@localhost:3306/Delilah');
+ 
+// ----------------------------- MIDDLEWARE -----------------------------------
+// GENERALES
+// Que no hayan parámetros de más en la ruta o path: 
+function validarRuta(req, res, next) {
+    console.log(req.query);
+    console.log(req.body);
+
+    if (Object.keys(req.query).length > 0) {
+        res.statusCode = 400;
+        res.json("Parámetros de más en la ruta");
+    } else {
+        next();
+    }
+}
+server.use(bodyParser.json());
+server.use(validarRuta);
+
+
+
+// ESPECÍFICOS
+
+// function validarBody(req, res, next) {
+//     // server.use(bodyParser.json());
+//     if(true) {
+//         // AQUI IRIA ALGO INDICANDO QUE NO SE CUMPLE CON LAS CONDICIONES
+//         // res.json("Acceso denegado!!!");
+//     } else {
+//     // AQUI IRIAN LAS FUNCIONES PORQUE SE CUMPLE CON LAS CONDICIONES
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// ----------------------------- TOKEN -----------------------------------
+function validarUsuario(req, res, next) {
+
+    let token = req.headers.authorization.split(" ")[1];
+    let validUser = jwt.verify(token, firma)
+
+    // // EN EL IF, FALTA COMPARAR EL USUARIO Y EL PASSWORD CON LA BASE DE DATOS 
+    if(validUser) {
+        res.locals.validUser = validUser;
+        next();
+    } else {
+        res.status(400)
+        res.json({error: "Token inválido"});
+    }
+    console.log(`Acceso permitido ${validUser}`);
+    next();
+}
+
+
+
+
+
+
+
+
+
 
 // ----------------------------- USUARIOS -----------------------------------
 
-let users;
 
-// Obtener el listado clientes
-server.get('/usuarios', (req, res) => {
-
-    if (users == true) {
-        res.json(users);
-        console.log(users);
+// OBTENER LISTA DE PRODUCTOS SIN VALIDACION (FUNCIONA PERFECTO)
+server.get('/usuarios/', (req, res) => {
+    if(sequelize) {
+        res.status(200);
+        sequelize.query('select * From cliente',
+            { type: sequelize.QueryTypes.SELECT }
+        ).then(function (resultados) {
+            res.json(resultados)
+        });
     } else {
-        res.json("No hay usuarios registrados");
-        console.log("No hay usuarios registrados");
+        res.status(404);
+        res.json({Error: "No hay usuarios registrados"});
     }
 });
 
-// Crear un usuario - Ruta /usuarios
-server.post('/usuarios', (req, res) => {
-    let newUser = req.body;
-    users.push(newUser);
-    console.log('Se agregó un nuevo cliente: ' + JSON.stringify(newUser));
-    res.status(201).send(req.body);
-})
+// OBTENER USUARIOS POR SU NOMBRE CON QUERY (ESTO PUEDE IR PARA UNA LUPA PARA BUSCAR PRODUCTO)
+server.get('/usuarios/', (req, res) => {
+    let nombreRecibido = req.query.nombre;
 
-server.post('/login', (req, res) => {
-    // email 
-    const {user, password} = req.body;
+    sequelize.query('select * From cliente where nombre = :nombre',
+    { replacements: {nombre: nombreRecibido}, type: sequelize.QueryTypes.SELECT }
+    ).then(function (result) {
+        res.status(200).send({
+            result
+        });
+    });
+});
+// RESUMIR ESTOS DOS PRIMEROS DE ARRIBA EN UNO SOLO 
+
+
+
+// AGREGAR USUARIOS SIN VALIDACION (FUNCIONA PERFECTO)
+server.post('/usuarios/', (req, res) => {
+    const {usuario, password, nombre, email, telefono, direccion} = req.body;
+    sequelize.query("INSERT INTO cliente (usuario, password, nombre, email, telefono, direccion) VALUES (?, ?, ?, ?, ?, ?)",
+    {replacements: [usuario, password, nombre, email, telefono, direccion]}
+        ).then((resultados) => {
+            console.log(`Usuario agregado con éxito ${resultados}`)
+            res.json('Agregado con éxito');
+        });
 });
 
+// ACTUALIZAR USUARIOS SIN VALIDACION (NO FUNCIONA)
+server.put('/usuarios/', (req, res) => {
+
+});
+
+// OBTENER USUARIOS POR SU NOMBRE EXACTO (ME FALTA FILTRAR QUE DE ERROR CUANDO NO HAY ID CARGADO) NO CREO NECESITAR ESTE
+server.get('/usuarios/:nombre', (req, res) => {
+    let nombreRecibido = req.params.nombre;
+
+    sequelize.query('select * From cliente where nombre = :nombre',
+    { replacements: {nombre: nombreRecibido}, type: sequelize.QueryTypes.SELECT }
+    ).then(function (result) {
+        res.status(200).send({
+            result
+        });
+    });
+});
+
+// OBTENER USUARIOS POR SU ID (ME FALTA FILTRAR QUE DE ERROR CUANDO NO HAY ID CARGADO)
+server.get('/usuarios/:id', (req, res) => {
+    let idUsuario = req.params.id;
+
+    sequelize.query('select * From cliente where id = :id',
+    { replacements: {id: idUsuario}, type: sequelize.QueryTypes.SELECT }
+    ).then(function (result) {
+        res.status(200).send({
+            result
+        });
+    });
+});
+
+////////////////////////////////////////
 
 
 
-// ----------------------------- PEDIDOS -----------------------------------
+
+
+
+
+let firma = "clave";
+
+server.post('/login', (req, res) => {
+    const {user, password} = req.body;
+
+    if(user == "zelevf" && password == "Ale12345") {
+        let token = jwt.sign(user, firma);
+        res.json(token);
+    } else {
+        res.json({error: "Datos de acceso incorrectos"});
+    }
+
+})
+
+server.post('/home', validarUsuario, (req, res) => {
+    res.send(`Hola ${res.locals.validUser}, bienvenido`);
+})
+
+
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
@@ -46,100 +198,59 @@ server.post('/login', (req, res) => {
 
 // ----------------------------- PRODUCTOS -----------------------------------
 
-// Obtener productos
-server.get('/productos', (req, res) => {
-    sequelize.query("select * from productos",
-    {type: sequelize.QueryTypes.SELECT}
-    ).then((resultados) => {
-        res.json(resultados);
-    })
-});
-
-
-// Obtener productos por su nombre (ESTO FUNCIONA PERFECTO)
-server.get('/productos/:nombre', (req, res) => {
-    const nombre = req.params.nombre;
-
-    sequelize.query("select * from productos where nombre = :nombre",
-    {replacements: {nombre}, type: sequelize.QueryTypes.SELECT}
-    ).then((resultados) => {
-        res.json(resultados);
-    })
-
-});
-
-// Agregar productos como administrador y funciona perfecto (creo que le faltaria el middleware)
-// server.post('/productos', (req, res) => {
-//     const {nombre, descripcion, precio, stock} = req.body;
-//     if(!nombre || !descripcion || !precio || !stock) {
-//         res.status(404).send({err: "Faltan parámetros"});
-//     } else {
-//         sequelize.query("insert into productos (nombre, descripcion, precio, stock) value (?,?,?,?)", 
-//         {replacements: [nombre, descripcion, precio, stock]}
-//         ).then(function(result) {
-//             res.status(202).send({producto: result})
-//         })
-//     }
-// });
-
-server.post('/productos', (req, res) => {
-    sequelize.query("insert into productos (nombre, descripcion, precio, stock) value (?,?,?,?)",
-        {replacements: [nombre, descripcion, precio, stock]}
-    ).then(function(result) {
-        console.log(result);
-        res.status(202).send({producto: result})
-    })
-});
-
-
-// ---------------------------- CARRITO -----------------------------------
-
-
-// ----------------------------- ÓRDENES -----------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-// ---------- FALTA POR ORDENAR -------------------
-
-// Creando una cuenta y agregando un cliente (creo que le faltaria el middleware)
-server.post('/cliente', (req, res) => {
-    console.log(req.body);
-    const {usuario, nombre, apellido, email, telefono, direccion, contraseña} = req.body;
-    sequelize.query('isert into clientes (usuario, nombre, apellido, email, telefono, direccion, contraseña) values (?,?,?,?,?,?,?)',   
-        {replacements: [usuario, nombre, apellido, email, telefono, direccion, contraseña]}
-        ).then ((respuesta) => {
-            console.log(respuesta)
-            res.sendStatus(200);
-        }); 
-})
-
-
-// Login (creo que le faltaria el middleware)
-server.post('/login', (req, res) => {
-    console.log(req.body);
-    const usuario = req.body;
-    const email = req.body;
-    const contraseña = req.body;
-
-    if ((usuario == "zelevf" || email == "fernandoveleze@gmail.com") && contraseña == "A.123456") {
-        res.json("Acceso correcto")
+// OBTENER LISTA DE PRODUCTOS SIN VALIDACION (FUNCIONA PERFECTO)
+server.get('/productos/', (req, res) => {
+    if(sequelize) {
+        res.status(200);
+        sequelize.query('select * From Productos',
+            { type: sequelize.QueryTypes.SELECT }
+        ).then(function (resultados) {
+            res.json(resultados)
+        });
     } else {
-        res.sendStatus(401);
-        res.json("Usuario o clave incorrecta");
+        res.status(404);
+        res.json({Error: "No hay productos registrados"});
     }
-})
+});
+
+// OBTENER PRODUCTOS POR SU ID SIN VALIDACION (FUNCIONA, PERO FALTA QUE CUANDO NO CONSIGA EL ID DE ERROR)
+server.get('/productos/:id', (req, res) => {
+    let idProductos = req.params.id;
+
+    sequelize.query('select * From productos where idProductos = :id',
+    { replacements: {id: idProductos}, type: sequelize.QueryTypes.SELECT }
+    ).then(function (result) {
+        res.status(200).send({
+            result
+        });
+    });
+});
+
+
+// AGREGAR PRODUCTOS SIN VALIDACION (FUNCIONA PERFECTO)
+server.post('/productos/', (req, res) => {
+    const {nombre, descripcion, precio, stock} = req.body;
+    sequelize.query("INSERT INTO productos (nombre, descripcion, precio, stock) VALUES (?, ?, ?, ?)",
+    {replacements: [nombre, descripcion, precio, stock]}
+        ).then((resultados) => {
+            console.log(`Producto agregado con éxito ${resultados}`)
+            res.json('Agregado con éxito');
+        });
+});
+
+// -----------------------------------------------------------------------
 
 
 
 
+server.use((err, req, res, next) => {
+    if(!err) {
+        next();
+    } else {
+    console.log('Error, algo salió mal', err);
+    res.status(500).send('Error');
+    }
+});
 
-server.listen(8080, () => console.log('Servidor iniciado, puerto 8080.'));
+
+server.listen(8080, () => console.log('Servidor iniciado, puerto 8080.')); 
